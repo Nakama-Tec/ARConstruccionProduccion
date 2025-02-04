63fb913a651467bec6bce231cea9d0374bd21422
Luciano Alberto Gatti <126739839+lucianodevs@users.noreply.github.com>
2025-02-04 05:08:27 -0300

    Update Config.js

--- DB/Config.js
+++ DB/Config.js
@@ -1,16 +1,18 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv')
+require('dotenv').config();
 
-dotenv.config()
+const mysql = require('mysql');
 
-const conection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//    queueLimit: 0
-})
+});
 
-module.exports = {conection}
+connection.connect((err) => {
+    if (err) {
+        console.error('❌ Error al conectar a MySQL:', err);
+        return;
+    }
+    console.log('✅ Conectado a MySQL en la VPS');
+});
