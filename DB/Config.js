125aad771c85e78ab3ef6241e62c1a3366a30ae3
Luciano Alberto Gatti <126739839+lucianodevs@users.noreply.github.com>
2025-02-04 04:12:39 -0300

    Update Config.js

--- DB/Config.js
+++ DB/Config.js
@@ -1,18 +1,13 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv')
-
-dotenv.config()
 
 const conection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: 3306 //puerto mysql 
+    host: 'localhost', // O la IP del servidor si no es localhost
+    user: 'root', // Sustituye con tu nombre de usuario
+    password: 'NakamaConstruccion', // Contraseña directa
+    database: 'DB_Construcciones3', // Nombre de la base de datos
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
+});
 
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//    queueLimit: 0
-})
-
-module.exports = {conection}
+module.exports = { conection };
