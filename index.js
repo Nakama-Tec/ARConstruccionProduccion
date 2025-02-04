7c4ebb8801b7b1ecc3d4f31ab09bffbfc61109a5
Luciano Alberto Gatti <126739839+lucianodevs@users.noreply.github.com>
2025-02-04 16:48:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-const {conection} = require('./DB/Config')
+const {conection} = require('./testDB')
 const alquilerDepartamentos = require("./Routes/alquilerDepartamentos")
 const cashFlow = require("./Routes/cashFlow")
 const certificados = require("./Routes/certificados")
